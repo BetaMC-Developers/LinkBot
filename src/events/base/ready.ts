5a199a0f8f65bfd97c176c1160ae7ffc9faa8cf2
1blckhrt <williams.1691@wright.edu>
2025-01-25 21:57:26 -0500

    refactor: switch to folderwatch

--- src/events/base/ready.ts
+++ src/events/base/ready.ts
@@ -2,10 +2,10 @@ import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 import process from "node:process";
-import { setInterval } from "node:timers";
 import { Events } from "discord.js";
 import { load } from "js-yaml";
 import { client } from "../../util/constants/client.js";
+import FolderWatcher from "../../util/constants/folderWatcher.js";
 import logger from "../../util/constants/logger.js";
 import { BOT_STATUS } from "../../util/constants/status.js";
 import type { Event } from "../../util/types/event.js";
@@ -41,14 +41,13 @@ async function loadYAMLFile() {
 				const member = guild.members.cache.get(id);
 
 				if (member && member.nickname !== username) {
-					console.log(`Found member: ${username}`);
 					await member.setNickname(username);
-					console.log(`Set nickname to: ${username}`);
+					console.log(`Set ${member.user.tag}'s nickname to ${username}`);
 				}
 
 				if (member && !member.roles.cache.has(CONFIG.memberRole)) {
 					await member.roles.add(CONFIG.memberRole);
-					console.log(`Added role: ${CONFIG.memberRole}`);
+					console.log(`Added ${member.user.tag} to the member role`);
 				}
 
 				processedUserIds.add(id);
@@ -65,9 +64,12 @@ export default {
 	async execute(client) {
 		client.user.setStatus(BOT_STATUS.Online);
 		logger.success(`Logged in as ${client.user.tag}`);
-		void loadYAMLFile();
-		setInterval(() => {
-			void loadYAMLFile();
-		}, 60_000);
+
+		await loadYAMLFile();
+		const watcher = new FolderWatcher(path.dirname(filePath), false);
+		watcher.onChange = async () => {
+			console.log("YML file changed");
+			await loadYAMLFile();
+		};
 	},
 } as const satisfies Event<Events.ClientReady>;
---
+++ src/util/constants/folderWatcher.ts
@@ -0,0 +1,120 @@
+/* eslint-disable n/no-sync */
+import fs from "node:fs";
+
+enum ItemType {
+	FILE = 0,
+	DIRECTORY = 1,
+	SYMLINK = 2,
+	UNKNOWN = 3,
+}
+
+export default class FolderWatcher {
+	public folder: string;
+
+	public recursive: boolean;
+
+	public onAdd: ((filePath: string, type: ItemType) => void) | null;
+
+	public onChange: ((filePath: string) => void) | null;
+
+	public onRemove: ((filePath: string) => void) | null;
+
+	public watchers: Map<string, fs.FSWatcher>; // path -> watcher
+
+	public constructor(folder: string, recursive = true) {
+		this.watchers = new Map();
+
+		this.folder = folder;
+		this.recursive = recursive;
+
+		this.onAdd = null;
+		this.onRemove = null;
+		this.onChange = null;
+
+		this.addWatcher(folder);
+	}
+
+	public Destroy() {
+		for (const watcher of this.watchers.values()) {
+			watcher.close();
+		}
+
+		this.onAdd = null;
+		this.onRemove = null;
+		this.onChange = null;
+		this.watchers.clear();
+	}
+
+	public GetItemType(file: string): ItemType {
+		try {
+			const stats = fs.lstatSync(file);
+			if (stats.isFile()) return ItemType.FILE;
+			if (stats.isDirectory()) return ItemType.DIRECTORY;
+			if (stats.isSymbolicLink()) return ItemType.SYMLINK;
+		} catch (error) {
+			console.error(`Error getting item type for file: ${file}`);
+			console.error(error);
+		}
+
+		return ItemType.UNKNOWN;
+	}
+
+	private Add(file: string) {
+		if (this.recursive && fs.lstatSync(file).isDirectory()) {
+			this.addWatcher(file);
+		}
+
+		if (this.onAdd) this.onAdd(file, this.GetItemType(file));
+	}
+
+	private Remove(file: string) {
+		for (const [path, watcher] of this.watchers) {
+			if (path.startsWith(file)) {
+				watcher.close();
+				this.watchers.delete(path);
+			}
+		}
+
+		if (this.onRemove) this.onRemove(file);
+	}
+
+	private Change(file: string) {
+		if (this.onChange) this.onChange(file);
+	}
+
+	public WatcherEvent(path: string, event: "change" | "rename", filename: string | null) {
+		if (!filename) {
+			console.warn(`Filename is null for path: ${path}`);
+			return;
+		}
+
+		const fullPath = `${path}/${filename}`;
+		if (event === "change") {
+			this.Change(fullPath);
+		} else if (event === "rename") {
+			if (fs.existsSync(fullPath)) {
+				this.Add(fullPath);
+			} else {
+				this.Remove(fullPath);
+			}
+		}
+	}
+
+	public addWatcher(path: string) {
+		if (this.watchers.has(path)) return;
+		const watcher = fs.watch(path, this.WatcherEvent.bind(this, path));
+		this.watchers.set(path, watcher);
+
+		if (!this.recursive) return;
+
+		const stats = fs.lstatSync(path);
+		if (stats.isDirectory()) {
+			const items = fs.readdirSync(path, { withFileTypes: true });
+			for (const item of items) {
+				if (item.isDirectory()) {
+					this.addWatcher(`${path}/${item.name}`);
+				}
+			}
+		}
+	}
+}
